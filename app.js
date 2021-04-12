83447b0278a47f99c74bbb6bb9696136bc62dba0
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2021-04-12 22:19:24 +0200

    done

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const Person = (props) => {
   return (
     <li>
       {props.person}
-      <button onClick={props.handleClick(props.person)}>Usuń</button>
+      <button onClick={props.handleClick}>Usuń</button>
     </li>
   );
 };
@@ -12,16 +12,18 @@ class List extends React.Component {
     people: ["Jan K.", "Piotr C.", "Marysia W.", "John S."],
   };
 
-  handleClick = (person) => {
-    console.log(person);
-    // this.setState({
-    //   people: this.state.people.splice(index, 1),
-    // });
+  handleClick = (index) => {
+    let people = this.state.people;
+    people.splice(index, 1);
+
+    this.setState({
+      people,
+    });
   };
 
   render() {
     const people = this.state.people.map((person, index) => (
-      <Person key={index} person={person} handleClick={this.handleClick} />
+      <Person key={index} person={person} handleClick={this.handleClick.bind(this, index)} />
     ));
 
     return <ul>{people}</ul>;
