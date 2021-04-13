68fdcd114aa2047f5bbf51bc5b61555eea36bdd2
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2021-04-13 22:24:41 +0200

    array to object

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const Person = (props) => {
   return (
     <li>
-      {props.person}
+      <span>{props.name}</span>
       <button onClick={props.handleClick}>Usuń</button>
     </li>
   );
@@ -9,7 +9,13 @@ const Person = (props) => {
 
 class List extends React.Component {
   state = {
-    people: ["Jan K.", "Piotr C.", "Marysia W.", "John S."],
+    // people: ["Jan K.", "Piotr C.", "Marysia W.", "John S."],
+    people: [
+      { id: 1, name: "Jan K." },
+      { id: 2, name: "Piotr C." },
+      { id: 3, name: "Marysia W." },
+      { id: 4, name: "John S." },
+    ],
   };
 
   handleClick = (index) => {
@@ -23,7 +29,7 @@ class List extends React.Component {
 
   render() {
     const people = this.state.people.map((person, index) => (
-      <Person key={index} person={person} handleClick={this.handleClick.bind(this, index)} />
+      <Person key={person.id} name={person.name} handleClick={this.handleClick.bind(this, index)} />
     ));
 
     return <ul>{people}</ul>;
